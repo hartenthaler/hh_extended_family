af86f9243c1707ee8eb250d05afe35df012ee855
Hermann Hartenthaler <hermann@hartenthaler.de>
2021-07-11 16:48:29 +0200

    new release: updated translations

--- module.php
+++ module.php
@@ -69,7 +69,7 @@ class ExtendedFamilyTabModule extends AbstractModule implements ModuleTabInterfa
     
     public const CUSTOM_WEBSITE = 'https://github.com/hartenthaler/' . self::CUSTOM_MODULE . '/';
     
-    public const CUSTOM_VERSION = '2.0.16.26';
+    public const CUSTOM_VERSION = '2.0.16.27';
 
     public const CUSTOM_LAST = 'https://github.com/hartenthaler/' . self::CUSTOM_MODULE. '/raw/main/latest-version.txt';
 
